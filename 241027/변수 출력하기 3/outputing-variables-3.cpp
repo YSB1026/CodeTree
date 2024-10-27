72883b3a3b9a44d210f088d21e50581c7df0ea44
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2024-10-27 19:09:34 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241027/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    char a = 1, b = 2, c = 'C';
+    printf("%d->%d->%c",a,b,c);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
